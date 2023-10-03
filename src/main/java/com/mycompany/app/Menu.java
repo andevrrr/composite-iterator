d2c1097c300b93e869039e386fe08c74566f17d3
Anton <kucherenko-anton1@mail.ru>
2023-10-03 23:02:31 +0300

    composite menu

--- src/main/java/com/mycompany/app/Menu.java
+++ src/main/java/com/mycompany/app/Menu.java
@@ -1,9 +1,42 @@
 package com.mycompany.app;
 
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.List;
 
-public interface Menu {
+public class Menu extends MenuComponent{
     
-    public Iterator<MenuItem> createIterator();
+    List<MenuComponent> menuComponent = new ArrayList<MenuComponent>();
+    String name;
+    String description;
 
+    public Menu(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public void add(MenuComponent menuComponent) {
+        menuComponent.add(menuComponent);
+    }
+
+    public void remove(MenuComponent menuComponent) {
+        menuComponent.remove(menuComponent);
+    }
+
+    public MenuComponent getChild(int i) {
+        return menuComponent.get(i);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void print() {
+        System.out.println("\n" + getName());
+        System.out.println(", " + getDescription());
+        System.out.println("---------------------");
+    }
 }
