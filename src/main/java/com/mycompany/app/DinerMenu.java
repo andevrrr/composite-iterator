7dff06209b7818333df0413aaa3a6d58d20586b9
Anton <kucherenko-anton1@mail.ru>
2023-10-03 18:48:50 +0300

    DinerMenu set up

--- src/main/java/com/mycompany/app/DinerMenu.java
+++ src/main/java/com/mycompany/app/DinerMenu.java
@@ -1,5 +1,29 @@
 package com.mycompany.app;
 
 public class DinerMenu {
-    
+    static final int MAX_ITEMS = 6;
+    int numberOfItems = 0;
+    MenuItem[] menuItems;
+
+    public DinerMenu() {
+        menuItems = new MenuItem[MAX_ITEMS];
+
+        addItem("Vegetarian BlT", "Vegetarian bacon with lettuce & tomato on whole wheat", true, 3.99);
+        addItem("BLT", "Bacon with lettuce & tomato on whole wheat", false, 4.99);
+
+    }
+
+    public void addItem(String name, String description, boolean vegetarian, double price) {
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        if (numberOfItems >= MAX_ITEMS) {
+            System.err.println("Sorry, menu is full! Can't add item to menu");
+        } else {
+            menuItems[numberOfItems] = menuItem;
+            numberOfItems = numberOfItems + 1;
+        }
+    }
+
+    public MenuItem[] getMenuItems() {
+        return menuItems;
+    }
 }
