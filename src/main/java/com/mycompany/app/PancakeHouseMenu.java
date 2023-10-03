e2ba7ce12c034bb76a0bd3bc7034605f36b1ba99
Anton <kucherenko-anton1@mail.ru>
2023-10-03 18:43:20 +0300

    PancakeHouseManu set up

---
+++ src/main/java/com/mycompany/app/PancakeHouseMenu.java
@@ -0,0 +1,25 @@
+package com.mycompany.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PancakeHouseMenu {
+    List<MenuItem> menuItems;
+
+    public PancakeHouseMenu() {
+        menuItems = new ArrayList<MenuItem>();
+
+        addItem("K&B's Pancake Breakfast", "Pancakes with scrambled eggs and toast", true, 2.99);
+        addItem("Regular Pancake Breakfast", "Pancakes with fried eggs, sausage", false, 2.99);
+
+    }
+
+    public void addItem(String name, String description, boolean vegetarian, double price) {
+        MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+        menuItems.add(menuItem);
+    }
+
+    public List<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+}
